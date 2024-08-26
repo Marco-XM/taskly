e2d286d0763da8eb2fc9dec1b72e6ad7b1a286a0
amir413 <amir.emad2007@gmail.com>
2024-08-26 20:43:28 +0300

    sds

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/app" element={<WorkSpace />} />
+      <Route path="/appp" element={<WorkSpace />} />
       <Route path="/dashboard" element={<DashBoard />} />
     </Routes>
   );
