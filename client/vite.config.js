4122e6a466babda086134f3bbb4ea736d280015b
marco <marcoelgawli66@gmail.com>
2024-09-11 09:20:58 +0300

    fixing reload

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://taskly-ozmg.vercel.app/',  // Ensure the base path is set to root for local dev
+  base: '/',  // Ensure the base path is set to root for local dev
   plugins: [react()],
 });
