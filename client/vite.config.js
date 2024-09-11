d46cb44943cf7ce69ba19b043d36e033bfc65037
marco <marcoelgawli66@gmail.com>
2024-09-11 22:55:55 +0300

    fixing reload

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // Ensure the base path is set to root for local dev
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This should match the folder used by Vercel
+    rollupOptions: {
+      output: {
+        entryFileNames: 'index-[hash].js',  // Ensure JS files are correctly hashed and outputted
+        chunkFileNames: 'chunk-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
 });
