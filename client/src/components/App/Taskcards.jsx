274898c3ee9232d208f8724a26f9d4c7d3b9eafe
marco <marcoelgawli66@gmail.com>
2024-09-17 13:44:22 +0300

    fixings

--- client/src/components/App/Taskcards.jsx
+++ client/src/components/App/Taskcards.jsx
@@ -680,7 +680,7 @@ const Taskcards = ({ onCloseModal }) => {
     //         handleDropOnBox(e, dropBoxIndex, dropTaskIndex);
     //     }
     // };
-    const handleDropOnTask = async (e, dropBoxIndex, dropTaskIndex) => {
+    const handleDropOnTask = async (boxes, e, dropBoxIndex, dropTaskIndex) => {
         e.preventDefault();
     
         const dragBoxIndex = parseInt(e.dataTransfer.getData('boxIndex'), 10);
