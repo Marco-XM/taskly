fa92fd8b7f47998521004dd69bc626692a4145a1
marco <marcoelgawli66@gmail.com>
2024-09-18 18:28:44 +0300

    secure routes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,46 +20,9 @@ const handleSubmit = async (event) => {
     const { email, password } = data;
 
     console.log('Submitting login with:', { email, password });
-    const base64UrlDecode = (str) => {
-        // Convert Base64Url to Base64
-        let base64 = str.replace(/-/g, '+').replace(/_/g, '/');
-        // Pad Base64 string if necessary
-        while (base64.length % 4 !== 0) {
-            base64 += '=';
-        }
-        // Decode Base64 string to a string
-        return atob(base64);
-        };
-    
-        const decodeJwt = (token) => {
-        if (!token) {
-            throw new Error('No token provided');
-        }
-    
-        const parts = token.split('.');
-        if (parts.length !== 3) {
-            throw new Error('JWT does not have 3 parts');
-        }
-    
-        const payload = parts[1];
-        const decodedPayload = base64UrlDecode(payload);
-        return JSON.parse(decodedPayload);
-        };
-    
-      // Helper function to get userId from the token
-        const getUserIdFromToken = () => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            const decoded = decodeJwt(token);
-          return decoded._id;  // Assuming your JWT contains the userId as _id
-        }
-        return null;
-        };
-    
-        const userId = getUserIdFromToken();
 
     try {
-        const response = await fetch('https://taskly-backend-one.vercel.app/login', {
+        const response = await fetch('https://taskly-backend-one.vercel.app/api/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, password })
@@ -72,7 +35,7 @@ const handleSubmit = async (event) => {
         if (response.ok) {
             localStorage.setItem('token', responseData.token);  // Store token
             console.log('Login successful:', responseData);
-            navigate(`/app/${userId}`);  // Navigate to the protected page after login
+            navigate('/app');  // Navigate to the protected page after login
         } else {
             console.error('Login failed:', responseData.error || 'Unknown error');
         }
