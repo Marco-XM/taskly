d49735bc4b9f77fbc2af79fa64c745903fb11353
marco <marcoelgawli66@gmail.com>
2024-09-19 14:11:09 +0300

    fixing routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -61,8 +61,11 @@ const App = () => {
       <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path={`/app/${userId}`} element={<PrivateRoute><WorkSpace /></PrivateRoute>} />
-        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="/app/:userId" element={
+        <PrivateRoute>
+            <WorkSpace />
+        </PrivateRoute>
+    } />        <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
         <Route path="/calendar" element={<CalendarPage/>}/>
