af8cad150b1fabb0da766f057bf90d60e5335c28
marco <marcoelgawli66@gmail.com>
2024-09-19 09:03:03 +0300

    secure routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,21 +18,33 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem(
 const PrivateRoute = ({ children }) => {
   return localStorage.getItem('token') ? children : <Navigate to="/login" />;
 };
+const base64UrlDecode = (str) => {
+  // Convert Base64Url to Base64
+  let base64 = str.replace(/-/g, '+').replace(/_/g, '/');
+  // Pad Base64 string if necessary
+  while (base64.length % 4 !== 0) {
+      base64 += '=';
+  }
+  // Decode Base64 string to a string
+  return atob(base64);
+};
+
+const decodeJwt = (token) => {
+  const parts = token.split('.');
+  if (parts.length !== 3) {
+      throw new Error('JWT does not have 3 parts');
+  }
+
+  const payload = parts[1];
+  const decodedPayload = base64UrlDecode(payload);
+  return JSON.parse(decodedPayload);
+};
+
+const token = localStorage.getItem('token');
+const decodedToken = decodeJwt(token);
+const userId = decodedToken._id;
 
 
-// const taskBoxes = JSON.parse(localStorage.getItem('taskBoxes'));
-
-// const saveTasksToDB = async (userId) => {
-//   try {
-//     const response = await axios.post('https://taskly-backend-one.vercel.app/api/tasks', {
-//       userId: userId,
-//       taskBoxes: taskBoxes
-//     });
-//     console.log('Tasks Saved To DB:', response.data);
-//   } catch (error) {
-//     console.error('Error Saving tasks to DB:', error);
-//   }
-// };
 
 
 
@@ -42,7 +54,7 @@ const App = () => {
       <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/app" element={<PrivateRoute><WorkSpace /></PrivateRoute>} />
+        <Route path={`/app/${userId}`} element={<PrivateRoute><WorkSpace /></PrivateRoute>} />
         <Route path="/dashboard" element={<DashBoard />} />
         <Route path="/register" element={<Registration />} />
         <Route path="/login" element={<Login />} />
